08d22673f90a237df4a8ce0cb4d32f680c729d3f
Ashesh Vidyut <134911583+absolutelightning@users.noreply.github.com>
2024-05-23 16:39:00 +0530

    Dev to Main (#17)
    * reverse iterator init
    
    * some fixes
    
    * fix reverse iterator seaklowerbound
    
    * fix tests
    
    * fix track channels
    
    * longest prefix on txn
    
    * some fixes
    
    * revisit
    
    * todo revisit
    
    * major code refactor
    
    * some minor fixes
    
    * add lru
    
    * fix bugs
    
    * added walk func
    
    * add more tests
    
    * some prog
    
    * some progress track mutate
    
    * some progress
    
    * fix tests
    
    * some optimizations
    
    * some memory optimizations
    
    * fix tests
    
    * minor fixes
    
    * some minor fixes
    
    * some fixes
    
    * fix delete prefix
    
    * some fixes
    
    * init stack
    
    * fix seek lower bound
    
    * clone
    
    * fix watch
    
    * fix clone bug
    
    * some fixes for go mem db
    
    * merge absl
    
    * Fix race
    
    * fix go mod
    
    * fix race in add child
    
    * fix last memdb test
    
    * clone
    
    * fix memdb last tests
    
    * fix race
    
    * Fix race without impacting memory and performance. Used node level lock
    
    * Add fix in seeklowerbound
    
    * minor check
    
    * fix race

--- node_4.go
+++ node_4.go
@@ -20,10 +20,14 @@ type Node4[T any] struct {
 }
 
 func (n *Node4[T]) getPartialLen() uint32 {
+	n.mu.Lock()
+	defer n.mu.Unlock()
 	return n.partialLen
 }
 
 func (n *Node4[T]) setPartialLen(partialLen uint32) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
 	n.partialLen = partialLen
 }
 
@@ -40,10 +44,14 @@ func (n *Node4[T]) setNumChildren(numChildren uint8) {
 }
 
 func (n *Node4[T]) getPartial() []byte {
+	n.mu.Lock()
+	defer n.mu.Unlock()
 	return n.partial
 }
 
 func (n *Node4[T]) setPartial(partial []byte) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
 	n.partial = partial
 }
 
