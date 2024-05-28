370ed6f94e40f4c51d8445763c0fa26b2da62314
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-05-28 14:23:39 +0530

    tracking channel of child changes

--- txn.go
+++ txn.go
@@ -355,6 +355,7 @@ func (t *Txn[T]) recursiveDelete(node Node[T], key []byte, depth int) (Node[T], 
 		if t.trackMutate {
 			t.trackChannel(oldRef)
 		}
+		t.trackChannel(child)
 		if doClone {
 			node = t.writeNode(node)
 		} else {
