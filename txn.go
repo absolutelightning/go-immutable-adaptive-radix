e47452a4014f1062c8d038e3ebb9a82275c6e521
Ashesh Vidyut <134911583+absolutelightning@users.noreply.github.com>
2024-05-29 03:40:34 +0530

    Dev to main (#38)
    * try fixing race
    
    * Revert "try fixing race"
    
    This reverts commit 02ff901eaa1f51d4c484e1377f4487c779fdc346.
    
    * using lock for set child and get child
    
    * using locks only for set child and get child
    
    * fix nil mu
    
    * fix delete
    
    * using locks to set attribute of node
    
    * fix memdb tests
    
    * tracking channel of child changes
    
    * removed process lazy redundant
    
    * fix nil check
    
    * Fix get watch bug
    
    * go fmt
    
    * some minor fixes
    
    * merge main

--- txn.go
+++ txn.go
@@ -351,16 +351,13 @@ func (t *Txn[T]) recursiveDelete(node Node[T], key []byte, depth int) (Node[T], 
 	newChild, val := t.recursiveDelete(child, key, depth+1)
 	if val != nil {
 
-		if t.trackMutate {
-			t.trackChannel(oldRef)
-		}
 
-		t.trackChannel(child)
+	t.trackChannel(child)
 
-		if doClone {
+	if doClone {
 			node = t.writeNode(node)
 		} else {
-			t.trackChannel(node)
+			t.trackChannel(oldRef)
 		}
 		node.setChild(idx, newChild)
 		if doClone {
