e69b528f0863a2762dc7a307aa488622c1bfe819
Ashesh Vidyut <134911583+absolutelightning@users.noreply.github.com>
2024-05-30 21:37:57 +0530

    Absl (#54)
    * fix go mod
    
    * Dev to Main (#39)
    
    * try fixing race
    
    * Revert "try fixing race"
    
    This reverts commit 02ff901eaa1f51d4c484e1377f4487c779fdc346.
    
    * using lock for set child and get child
    
    * using locks only for set child and get child
    
    * fix nil mu
    
    * fix delete
    
    * using locks to set attribute of node
    
    * fix memdb tests
    
    * tracking channel of child changes
    
    * removed process lazy redundant
    
    * fix nil check
    
    * Fix get watch bug
    
    * go fmt
    
    * some minor fixes
    
    * merge main
    
    * fix go mod
    
    * fix go mod
    
    * fmt
    
    * Dev to Main (#40)
    
    * try fixing race
    
    * Revert "try fixing race"
    
    This reverts commit 02ff901eaa1f51d4c484e1377f4487c779fdc346.
    
    * using lock for set child and get child
    
    * using locks only for set child and get child
    
    * fix nil mu
    
    * fix delete
    
    * using locks to set attribute of node
    
    * fix memdb tests
    
    * tracking channel of child changes
    
    * removed process lazy redundant
    
    * fix nil check
    
    * Fix get watch bug
    
    * go fmt
    
    * some minor fixes
    
    * merge main
    
    * fix go mod
    
    * fix go mod
    
    * some minor fixes
    
    * some minor fixes
    
    * fmt
    
    * Dev to main (#41)
    
    * try fixing race
    
    * Revert "try fixing race"
    
    This reverts commit 02ff901eaa1f51d4c484e1377f4487c779fdc346.
    
    * using lock for set child and get child
    
    * using locks only for set child and get child
    
    * fix nil mu
    
    * fix delete
    
    * using locks to set attribute of node
    
    * fix memdb tests
    
    * tracking channel of child changes
    
    * removed process lazy redundant
    
    * fix nil check
    
    * Fix get watch bug
    
    * go fmt
    
    * some minor fixes
    
    * merge main
    
    * fix go mod
    
    * fix go mod
    
    * some minor fixes
    
    * some minor fixes
    
    * fmt
    
    * some fixes
    
    * Dev to Main (#42)
    
    * try fixing race
    
    * Revert "try fixing race"
    
    This reverts commit 02ff901eaa1f51d4c484e1377f4487c779fdc346.
    
    * using lock for set child and get child
    
    * using locks only for set child and get child
    
    * fix nil mu
    
    * fix delete
    
    * using locks to set attribute of node
    
    * fix memdb tests
    
    * tracking channel of child changes
    
    * removed process lazy redundant
    
    * fix nil check
    
    * Fix get watch bug
    
    * go fmt
    
    * some minor fixes
    
    * merge main
    
    * fix go mod
    
    * fix go mod
    
    * some minor fixes
    
    * some minor fixes
    
    * fmt
    
    * some fixes
    
    * fix seek prefix watch
    
    * SeekPrefix Watch Fix (#43)
    
    * try fixing race
    
    * Revert "try fixing race"
    
    This reverts commit 02ff901eaa1f51d4c484e1377f4487c779fdc346.
    
    * using lock for set child and get child
    
    * using locks only for set child and get child
    
    * fix nil mu
    
    * fix delete
    
    * using locks to set attribute of node
    
    * fix memdb tests
    
    * tracking channel of child changes
    
    * removed process lazy redundant
    
    * fix nil check
    
    * Fix get watch bug
    
    * go fmt
    
    * some minor fixes
    
    * merge main
    
    * fix go mod
    
    * fix go mod
    
    * some minor fixes
    
    * some minor fixes
    
    * fmt
    
    * some fixes
    
    * fix seek prefix watch
    
    * bug fix
    
    * some fixes
    
    * track mutation fixes
    
    * fix iterator
    
    * fix itertor

--- path_iter.go
+++ path_iter.go
@@ -43,7 +43,7 @@ func (i *PathIterator[T]) Next() ([]byte, T, bool) {
 			continue
 		case node4:
 			n4 := currentNode.(*Node4[T])
-			for itr := 3; itr >= 0; itr-- {
+			for itr := int(n4.getNumChildren() - 1); itr >= 0; itr-- {
 				nodeCh := n4.children[itr]
 				if nodeCh == nil {
 					continue
@@ -56,7 +56,7 @@ func (i *PathIterator[T]) Next() ([]byte, T, bool) {
 			}
 		case node16:
 			n16 := currentNode.(*Node16[T])
-			for itr := 15; itr >= 0; itr-- {
+			for itr := int(n16.getNumChildren() - 1); itr >= 0; itr-- {
 				nodeCh := n16.children[itr]
 				if nodeCh == nil {
 					continue
@@ -69,7 +69,7 @@ func (i *PathIterator[T]) Next() ([]byte, T, bool) {
 			}
 		case node48:
 			n48 := currentNode.(*Node48[T])
-			for itr := 255; itr >= 0; itr-- {
+			for itr := int(n48.getNumChildren() - 1); itr >= 0; itr-- {
 				idx := n48.keys[itr]
 				if idx == 0 {
 					continue
@@ -86,7 +86,7 @@ func (i *PathIterator[T]) Next() ([]byte, T, bool) {
 			}
 		case node256:
 			n256 := currentNode.(*Node256[T])
-			for itr := 255; itr >= 0; itr-- {
+			for itr := int(n256.getNumChildren() - 1); itr >= 0; itr-- {
 				nodeCh := n256.children[itr]
 				if nodeCh == nil {
 					continue
